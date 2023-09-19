e49a19b102eeb0bd3d659a4dce3dbee3b162cb26
evotianusics <evo@impactcredit.solutions>
2023-09-19 11:23:51 +0700

    Commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://evotianusx.github.io',
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/evotianusx',
+				linkedin: 'https://www.linkedin.com/in/evotianusb',
+			},
+			sidebar: [
+				{
+					label: 'Home',
+					link: '/'
+				},
+				{
+					label: 'Resume',
+					link: '/resume'
+				},
+				{
+					label: 'Projects',
+					autogenerate: { directory: 'projects' },
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/projects/astro.md
@@ -0,0 +1,5 @@
+---
+title: Astro as a simple website
+description: A reference page in my new Starlight docs site.
+---
+Add in some github pages action
