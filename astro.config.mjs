ee8f43304465f8a3a35941a6558d5c09a4848a21
evotianusics <evo@impactcredit.solutions>
2023-09-19 15:35:45 +0700

    With Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://evotianusx.github.io',
-	integrations: [
-		starlight({
-
-			title: 'Evotianus Page',
-			social: {
-				github: 'https://github.com/evotianusx',
-				linkedin: 'https://www.linkedin.com/in/evotianusb',
-			},
-			sidebar: [
-				{
-					label: 'Home',
-					link: '/'
-				},
-				{
-					label: 'Resume',
-					link: '/resume'
-				},
-				{
-					label: 'Projects',
-					autogenerate: { directory: 'projects' },
-				},
-			],
-		}),
-	],
+  site: 'https://evotianusx.github.io',
+  integrations: [starlight({
+    title: 'Evotianus Page',
+    social: {
+      github: 'https://github.com/evotianusx',
+      linkedin: 'https://www.linkedin.com/in/evotianusb'
+    },
+    sidebar: [{
+      label: 'Home',
+      link: '/'
+    }, {
+      label: 'Resume',
+      link: '/resume'
+    }, {
+      label: 'Projects',
+      autogenerate: {
+        directory: 'projects'
+      }
+    }]
+  }), tailwind()]
 });
---
+++ src/content/docs/projects/home.md
@@ -0,0 +1,7 @@
+---
+title: Home
+description: ""
+
+sidebar:
+  order: 1
+---
